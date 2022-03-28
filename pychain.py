755f46340c682a6dff699d3993237551b86f6b9f
Matthew Guillen <matthewguillen777@gmail.com>
2022-03-27 23:11:19 -0400

    completed step 1-3

--- pychain.py
+++ pychain.py
@@ -49,7 +49,11 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -83,7 +87,7 @@ class Block:
 
         creator_id = str(self.creator_id).encode()
         sha.update(creator_id)
-
+    
         timestamp = str(self.timestamp).encode()
         sha.update(timestamp)
 
@@ -170,15 +174,15 @@ input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input("Sender")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input("Receiver")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input("Amount")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
